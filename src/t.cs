9cbdb938305fbde98f34b2d8d3bd8cc234bf7d43
cordis-dev <darius@adroiti.com>
2024-04-20 19:15:19 +0300

    Update t.cs

--- src/t.cs
+++ src/t.cs
@@ -5,23 +5,12 @@ public class Test
     public bool Enabled
     {
 	
-	get
-	{
-		
-		return true;
+		get
+		{
+			
+			return true;
 
-		
-	}
-    }
-
-    public bool Enabled2
-    {
-        get 
-        { 
-	    System.Console.WriteLine("Getting the enabled flag.");
-
-
-	    return false; 
-        }
+			
+		}
     }
 }
