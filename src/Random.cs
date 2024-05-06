3142e4fe2f923b1764e715490e62088cbde7f443
cordis-dev <darius@adroiti.com>
2024-05-06 13:21:53 +0300

    Create Random.cs

---
+++ src/Random.cs
@@ -0,0 +1,20 @@
+using System;
+using System.IO;
+
+class C
+{
+    public static string GetNameToCheck(string input)
+    {
+        if (input.Contains('\\', StringComparison.Ordinal))
+        {
+            return input.Substring(0, input.IndexOf("\\", StringComparison.Ordinal));
+        }
+
+        return Path.GetFileNameWithoutExtension(input);
+    }
+
+    public static bool IsValid(string input)
+    {
+        return input.StartsWith("{") && input.EndsWith('}');
+    }
+}
