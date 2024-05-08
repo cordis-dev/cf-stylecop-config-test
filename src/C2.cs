8f29ac732936b24c925e89fccaef480615133f21
cordis-dev <darius@adroiti.com>
2024-05-08 10:09:03 +0300

    Create C2.cs

---
+++ src/C2.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeAnalysisRuleTests
+{
+    internal class Class2
+    {
+        public bool Disabled
+        {
+            get
+            {
+
+                return false;
+            }
+        }
+    }
+}
