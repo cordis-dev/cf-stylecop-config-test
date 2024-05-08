7189f054f2346612439f8ebcd710cd27b6c2f5b4
cordis-dev <darius@adroiti.com>
2024-05-08 10:07:54 +0300

    Create C1.cs

---
+++ src/C1.cs
@@ -0,0 +1,37 @@
+using System;
+using System.IO;
+
+class C
+{
+    public bool Enabled
+    {
+
+        get
+        {
+
+            return true;
+        }
+    }
+
+    public static string GetNameToCheck(string input)
+    {
+        do
+        {
+            Console.WriteLine("Loop forever");
+        }
+
+        while (true);
+
+        if (input.Contains('\\', StringComparison.Ordinal))
+        {
+            return input.Substring(0, input.IndexOf("\\", StringComparison.Ordinal));
+        }
+
+        return Path.GetFileNameWithoutExtension(input);
+    }
+
+    public static bool IsValid(string input)
+    {
+        return input.StartsWith("{") && input.EndsWith('}');
+    }
+}
